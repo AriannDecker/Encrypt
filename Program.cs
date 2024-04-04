93c5552d404a74a7c7b976ca3dc5ac4b8424b2d8
Ariann Decker <adecker1207@gmail.com>
2024-04-04 18:58:57 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encrypt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What do you want to encrypt?");
+
+            var input = Console.ReadLine();
+
+
+        }
+        private static bool validation(string input)
+        {
+            if(string.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+            if (input.Length > 20)
+            {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
